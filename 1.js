7f77f700dae9e821a74a6a097fdee349b75650bf
karthik karthik <k64251501@gmail.com>
2023-02-27 22:40:11 +0530

    intial commit

---
+++ 1.js
@@ -0,0 +1,20 @@
+// for (let i=0;  ; i++){
+//     console.log('loop , i=' +i);
+//     if(i>1) break;
+// }
+ function check_num(num){
+        if (num % 7 ==0){
+         console.log('Divisible')
+        }else {
+            console.log('not Divisible')
+        }
+ }
+ function check_num(num){
+
+    if (num % 7 == 0){
+        console.log("Divisible")
+    }else{
+        console.log("Not Divisible")
+    }
+
+}
---
+++ two.js
@@ -0,0 +1,51 @@
+// // // // // // function check_num(num){
+
+// // // // // //     if (num % 7 !== 0){
+// // // // // //         console.log("Divisible")
+// // // // // //     }else{
+// // // // // //         console.log("Not Divisible")
+// // // // // //     }
+
+// // // // // // }
+// // // // // // check_num(7)
+// // // // // function check_num (x){
+// // // // //      if(x * 3 ==0){
+// // // // //         console.log("yes")
+// // // // //      }else{
+// // // // //         console.log('no')
+// // // // //      }
+// // // // // }
+// // // // // check_num(3)
+// // // // let num = 5;
+// // // // if(num > 0){
+// // // //    console.log('postive')
+// // // // }else{
+// // // //     console.log('negative')
+// // // // }
+// // // let num = 533;
+// // // num=String(num);
+// // // if(num.length ==3){
+// // //  console.log('yes')
+// // // }else{
+// // //     console.log('no')
+// // // }
+// // const check_even_odd=(num) =>{
+// //   if(num % 2==0){
+// //     console.log('even Number')
+// //   }else{
+// //     console.log('odd Number')
+// //   }
+// // }
+// // check_even_odd(14)
+// let n1=500;
+// let n2=80;
+// let n3=44;
+//  if(n1> n2 & n1 > n3){
+//      console.log(`${n1} is greater than${n2} and ${n3}`)
+//  }else if (n2> n1& n2>n3){
+//  console.log(`${n2} is greater than${n1} and ${n3}`)
+//  }else{
+//     console.log(`${n3} is greater than${n1} and ${n3}`)
+//  }
+let num = [5,6,1];
+console.log(num.sort())
